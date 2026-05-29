9843b790d61179c880299674ed763cdbb285fad9
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-05-29 13:14:25 +0000

    Auto-commit: 1 modified file

--- Python-Aditya/parking_management.py
+++ Python-Aditya/parking_management.py
@@ -1,23 +1,40 @@
 
 # parking management system
 
+parking = ["empty"] * 5
+
 print("welcome to the parking management system")
 print("----------------------------------------")
 
 
-name = input("inter your name   ")
+name = input("enter your name   ")
 
 car_number = input("enter your car number  ")
 
 
 while True:
-    print("\n 1 hour parking fee is $100")
-    print(" 2 hours parking fee is $150")
-    print(" 3 hours parking fee is $200")
-    print(" 24 hours parking fee is $1000")
+    
+    print("\n 1 hour parking fee is $10")
+    print(" 2 hours parking fee is $15")
+    print(" 3 hours parking fee is $20")
+    print(" 24 hours parking fee is $100")
 
     exit_time = input("how long can a car be parked in parking lot ")
 
-    if car_number == 1:
-        print("eudfhefs")
+    choice = input("select one number: ")
+
+    if choice == "1":
+
+        print("\ncheaking parking status")
+        print("we have 5 parking lots")
+
+        for i in range (len(parking)):
+            print(f"slot{i+1}: {parking[i]}")
+
+        else:
+         print("invalid choice")
+
+    break
+
+
 
