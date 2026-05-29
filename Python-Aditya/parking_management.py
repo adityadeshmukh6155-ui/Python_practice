102d965728ffc33afe7a6e5718339e0eac6b493c
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-05-29 07:56:53 +0000

    Auto-commit: 1 modified file

---
+++ Python-Aditya/parking_management.py
@@ -0,0 +1,19 @@
+
+# parking management system
+
+print("welcome to the parking management system")
+print("----------------------------------------")
+
+
+name = input("inter your name")
+
+car_number = input("enter your car number")
+
+
+while True:
+    print("\n 1 hour parking fee is $100")
+    print(" 2 hours parking fee is $150")
+    print(" 3 hours parking fee is $200")
+    print(" 24 hours parking fee is $1000")
+
+    exit_time = input("how long can a car be parked in parking lot")
