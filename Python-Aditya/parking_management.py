2aab3da256f197f5826da117a0a0f5faae419681
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-05-29 07:59:34 +0000

    --- Auto Git Commit ---

--- Python-Aditya/parking_management.py
+++ Python-Aditya/parking_management.py
@@ -5,9 +5,9 @@ print("welcome to the parking management system")
 print("----------------------------------------")
 
 
-name = input("inter your name")
+name = input("inter your name   ")
 
-car_number = input("enter your car number")
+car_number = input("enter your car number  ")
 
 
 while True:
@@ -16,4 +16,8 @@ while True:
     print(" 3 hours parking fee is $200")
     print(" 24 hours parking fee is $1000")
 
-    exit_time = input("how long can a car be parked in parking lot")
+    exit_time = input("how long can a car be parked in parking lot ",name)
+
+    if car_number == 1:
+        print("eudfhef")
+
