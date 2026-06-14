5be642c882cf828bfa43cbba86e6565d72280b87
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-06-14 07:06:19 +0000

    Auto-commit: 1 modified file

--- Python-Aditya/learning_files(input/output)/learning_open.py
+++ Python-Aditya/learning_files(input/output)/learning_open.py
@@ -1,6 +1,3 @@
-
-f = open(r"C:\Users\adity\Downloads\Aditya Deshmukh.txt","r")
-line1 = f.readlines()
-print(line1)
-print(type(line1))
-f.close()
+with open(r"C:\Users\adity\Downloads\Aditya Deshmukh.txt", "r") as f:
+    for line in f:
+        print(line.strip())
