4fceaca2251b2616b93adf47697f357b4cc17c0a
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-06-14 07:10:55 +0000

    --- Auto Git Commit ---

--- Python-Aditya/learning_files(input/output)/learning_open_write.py
+++ Python-Aditya/learning_files(input/output)/learning_open_write.py
@@ -1,4 +1,2 @@
-with open(r"C:\Users\adity\Downloads\Aditya Deshmukh.txt.txt", "w") as f:
-    f.write("Python\n")
-    f.write("SQL\n")
-    f.write("PySpark\n")
+with open(r"C:\Users\adity\Downloads\Aditya Deshmukh.txt", "w") as f:
+    f.write("Hello Aditya")
