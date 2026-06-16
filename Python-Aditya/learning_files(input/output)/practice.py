c4428bc72e4358a17d45c9150c3065b57ecfa6ed
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-06-16 11:26:19 +0000

    Auto-commit: 1 modified file

--- Python-Aditya/learning_files(input/output)/practice.py
+++ Python-Aditya/learning_files(input/output)/practice.py
@@ -1,7 +1,7 @@
 word = "learning"
 with open("Python-Aditya/learning_files(input/output)/practice.txt","r") as f:
      data =  f.read()
-     if(data.find(word) == -1):
+     if(data.find(word) != -1):
        print("found")
      else:
          print("not found")
