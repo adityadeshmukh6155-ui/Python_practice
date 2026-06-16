b188a96da30c25420bc2b08395ce11a5d1737b1e
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-06-16 11:25:35 +0000

    --- Auto Git Commit ---

--- Python-Aditya/learning_files(input/output)/practice.py
+++ Python-Aditya/learning_files(input/output)/practice.py
@@ -1,5 +1,7 @@
 word = "learning"
 with open("Python-Aditya/learning_files(input/output)/practice.txt","r") as f:
      data =  f.read()
-     if(data.find(word)):
-      print(len(word))
+     if(data.find(word) != -1):
+       print("found")
+     else:
+         print("not found")
