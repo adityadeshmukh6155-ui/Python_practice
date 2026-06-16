127741ccb1e1d94bc0fd3eb03d0529ef91fca57c
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-06-16 12:00:29 +0000

    Auto-commit: 1 modified file

--- Python-Aditya/learning_files(input/output)/new_prac.py
+++ Python-Aditya/learning_files(input/output)/new_prac.py
@@ -5,7 +5,7 @@ with("Python-Aditya/learning_files(input/output)/pract.txt","r") as f:
    nums = data.split(",")
    for val in nums:
       if(int(val) % 2 == 0):
-         print(count)
+         count += 1
 
 print(count)
 
