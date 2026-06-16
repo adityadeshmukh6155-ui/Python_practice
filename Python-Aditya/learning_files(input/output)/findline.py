1ed01bc3f0d91c0689dc740ffb2ffe5266634760
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-06-16 11:42:51 +0000

    Auto-commit: 1 modified file

--- Python-Aditya/learning_files(input/output)/findline.py
+++ Python-Aditya/learning_files(input/output)/findline.py
@@ -5,7 +5,7 @@ with open("Python-Aditya/learning_files(input/output)/practice.txt","r")as f:
  while data: 
   data = f.readline()
   if(word in data):
-   print("line_no")
+   print(line_no)
 
    line_no += 1
  
