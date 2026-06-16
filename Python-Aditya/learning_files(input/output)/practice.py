42a51c41c237ce9d1ce48c196fdab43c5276e487
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-06-16 07:09:55 +0000

    Auto-commit: 2 modified files

--- Python-Aditya/learning_files(input/output)/practice.py
+++ Python-Aditya/learning_files(input/output)/practice.py
@@ -1,6 +1,5 @@
 with open("Python-Aditya/learning_files(input/output)/practice.txt","w+")as f:
-  data =  f.read()
-
-data.replace("java","python")
+   f.write("hi everyone\n we are learning file i/o\n")
+   f.write("using java.\n i like pragramming in java.")
 f.close()
 
