66fa84628359e8c0a1838142548642e7a405c495
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-06-16 11:23:57 +0000

    Auto-commit: 1 modified file

--- Python-Aditya/learning_files(input/output)/practice.py
+++ Python-Aditya/learning_files(input/output)/practice.py
@@ -1,5 +1,5 @@
 word = "learning"
 with open("Python-Aditya/learning_files(input/output)/practice.txt","r") as f:
      data =  f.read()
-     if(data.find(word))
-     print(len(word))
+     if(data.find(word)):
+      print(len(word))
