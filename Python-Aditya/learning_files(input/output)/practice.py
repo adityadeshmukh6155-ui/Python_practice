e2f053386ed18fe13fc580cbac6ef2e51c703b24
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-06-16 11:23:35 +0000

    --- Auto Git Commit ---

--- Python-Aditya/learning_files(input/output)/practice.py
+++ Python-Aditya/learning_files(input/output)/practice.py
@@ -1,8 +1,5 @@
+word = "learning"
 with open("Python-Aditya/learning_files(input/output)/practice.txt","r") as f:
      data =  f.read()
-
-new_data = data.replace("java","python")
-print(new_data)
-
-with open("Python-Aditya/learning_files(input/output)/practice.txt","w") as f:
-     f.write(new_data)
+     if(data.find(word))
+     print(len(word))
