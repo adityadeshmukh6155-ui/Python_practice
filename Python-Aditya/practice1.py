d31cf449998a5da8e559153973d55f897609b02e
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-06-17 05:16:34 +0000

    Auto-commit: 1 modified file

--- Python-Aditya/practice1.py
+++ Python-Aditya/practice1.py
@@ -1,7 +1,17 @@
-number = 15
+# number = 15
 
-if number %2 == 0:
-    print("even")
+# if number %2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+salary = 60000
+
+if salary <= 50000:
+    print("bonus is 10%")
+
 else:
-    print("odd")
+    print("bonus is 5%")
+
 
