f911e8b42fe177def567635853c0623272ad9346
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-06-17 15:07:59 +0000

    Auto-commit: 1 modified file

--- Python-Aditya/practice1.py
+++ Python-Aditya/practice1.py
@@ -98,18 +98,26 @@
 # ==================================================================================================
 
 
-text = "data engineering data python"
-words =text.split()
-frequency = {}
+# text = "data engineering data python"
+# words =text.split()
+# frequency = {}
 
-for word in words:
-    if word in frequency:
-        frequency[word] += 1
-    else:
-        frequency[word] = 1
+# for word in words:
+#     if word in frequency:
+#         frequency[word] += 1
+#     else:
+#         frequency[word] = 1
 
-for word,count in frequency.items():
- print(f"{word}:{count}") 
+# for word,count in frequency.items():
+#  print(f"{word}:{count}") 
+# ==================================================================================================
+
+
+with open("Python-Aditya/learning_files(input/output)/pract.txt","r") as f:
+    data = f.read()
+    print(data)
+    f.close
+          
 
 
 
