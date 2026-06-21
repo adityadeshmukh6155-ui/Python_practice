de505df819dcb10a33078f702425f150055897f6
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-06-21 15:58:54 +0000

    Auto-commit: 1 modified file

--- Python-Aditya/practice1.py
+++ Python-Aditya/practice1.py
@@ -154,4 +154,4 @@ number = "+49 (176) 123-4567"
 print(number.replace("+","").replace(" ","").replace("-","").replace("(","").replace(")",""))
 
 number = "+49 (176) 123-4567"
-print(number.replace"+49 (176) 123-4567","491461234567")
+print(number.replace("+49 (176) 123-4567","491461234567"))
