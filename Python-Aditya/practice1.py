7156298eb983faa919a6cf8d04393df5a5658671
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-06-22 11:55:11 +0000

    Auto-commit: 1 modified file

--- Python-Aditya/practice1.py
+++ Python-Aditya/practice1.py
@@ -169,4 +169,4 @@
 # ===============================================================================================
 
 text = "968-Maria, ( Data engineer ) ;; 27y  "
-clean = text.replace("968","name").replace("(","|").replace(")",("|").replace(";;")(" "))
+clean = text.replace("968","name").replace("","|").replace("","|").replace(";;"," ")
