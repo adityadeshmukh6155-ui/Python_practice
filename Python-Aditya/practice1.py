80b4976fee0f92a1c28ceceec4cf7030dbc27f76
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-06-23 12:27:43 +0000

    Auto-commit: 1 modified file

--- Python-Aditya/practice1.py
+++ Python-Aditya/practice1.py
@@ -291,7 +291,7 @@
 # print("Total Salary:", total_salary)
 # ================================================================================================
 
-age  = input("enter your age:  ")
+age  = str(input("enter your age:  "))
 
 if age <= 18:
     print("you are minor")
