1d2061e77c95f236b33d04bfb43c0a6537567c7e
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-07-15 12:14:47 +0000

    chore: update Python-Aditya/practice1.py

--- Python-Aditya/practice1.py
+++ Python-Aditya/practice1.py
@@ -320,7 +320,7 @@
 #     print("You are a Senior Citizen")
 # =======================================================================================================
 
-a  = input(int("inter your age"))
+a  = input("inter your age")
 b = input(float("inter your height"))
 c = input("inter your name")
 d = input("are you stydent")
