46fc3945a4a3859e5c1006335fb4ad4b38176211
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-08-07 12:02:53 +0000

    chore: update Python-Aditya/practice1.py

--- Python-Aditya/practice1.py
+++ Python-Aditya/practice1.py
@@ -610,8 +610,11 @@ num1 = int(input("enter your number"))
 while True:
     if num1 % 10 == 0:
       print("result : " , num1)
+      break
     elif num1 // 10 == 0:
        print("result : " ,num1)
        break
+    else:
+       print("error")
       
       
