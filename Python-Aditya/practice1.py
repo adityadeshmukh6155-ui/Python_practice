a53e3da93d22b75b965aefe5467da66a76bd60dc
adityadeshmukh6155-ui <adityadeshmukh6155@gmail.com>
2026-08-07 09:52:10 +0000

    chore: update Python-Aditya/practice1.py

--- Python-Aditya/practice1.py
+++ Python-Aditya/practice1.py
@@ -573,8 +573,10 @@
 
 year= int(input("enter your year "))
 
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-     print("this is ")
+if (year % 4 == 0):
+    print("this is leap year", year)
+else:
+    print("this is not leap year")
 
 
 
